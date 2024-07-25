916aed5aac49030646798d9589843ef4f4462845
Daniel Halachev <dani22h09_pl@abv.bg>
2024-07-25 14:59:51 +0300

    Added results for HW14

--- SourceCode/src/AccelerationStructure.cpp
+++ SourceCode/src/AccelerationStructure.cpp
@@ -1,7 +1,13 @@
 #include "tree/AccelerationStructure.h"
 
+#include <chrono>
+#include <iostream>
+#include <mutex>
 #include <numeric>
 #include <stack>
+#include <thread>
+
+#include "threadpool/ThreadManager.h"
 
 TriangleKDTree::TriangleKDTree(Mesh &mesh, const unsigned short maxDepth, const unsigned short maxElementsInLeaf)
     : KDTree<Triangle>(maxDepth, maxElementsInLeaf) {
@@ -20,16 +26,27 @@ ObjectKDTree::ObjectKDTree() : KDTree<ObjectKDTreeSubTree>(){};
 
 ObjectKDTree::ObjectKDTree(Scene &scene, const unsigned short maxDepth, const unsigned short maxElementsInLeaf)
     : KDTree<ObjectKDTreeSubTree>(maxDepth, maxElementsInLeaf) {
+  // auto startTime = std::chrono::high_resolution_clock::now();
   std::vector<ObjectKDTreeSubTree> *subTrees = new std::vector<ObjectKDTreeSubTree>;
   subTrees->reserve(scene.objects.size());
+  ThreadManager manager(std::thread::hardware_concurrency());
+  std::mutex mutex;
   for (auto &object : scene.objects) {
-    subTrees->push_back(ObjectKDTreeSubTree(object));
+    manager.doJob([subTrees, &object, &mutex]() {
+      ObjectKDTreeSubTree subTree(object);
+      std::lock_guard<std::mutex> lock(mutex);
+      subTrees->push_back(subTree);
+    });
   }
+  manager.waitForAll();
   this->container = subTrees;
   unsigned int rootIndex = this->createNode(BoundingBox(scene), INVALID_INDEX, INVALID_INDEX, INVALID_INDEX);
   std::vector<size_t> indexes(scene.objects.size());
   std::iota(indexes.begin(), indexes.end(), 0);
   build(rootIndex, 0, indexes);
+  // auto endTime = std::chrono::high_resolution_clock::now();
+  // std::chrono::duration<double> duration = endTime - startTime;
+  // std::cout << duration.count() << "s\n";
 }
 
 ObjectKDTree::~ObjectKDTree() {
